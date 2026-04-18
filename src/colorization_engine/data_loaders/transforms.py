72ca48f65b84a1d43d7c5a011a32457b17ea794c
KlymentievMaksym <dadmon1901@gmail.com>
2026-04-18 16:28:32 +0000

    feat(transforms): add base transforms

---
+++ src/colorization_engine/data_loaders/transforms.py
@@ -0,0 +1,24 @@
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+
+def get_transforms(image_size: int = 256, is_train: bool = True) -> A.Compose:
+    pipeline = [
+        A.LongestMaxSize(max_size=image_size),
+        A.PadIfNeeded(min_height=image_size, min_width=image_size)
+    ]
+
+    if is_train:
+        pipeline.extend([
+            A.HorizontalFlip(p=0.5),
+        ])
+
+    pipeline.extend([
+        A.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)),
+        ToTensorV2()
+    ])
+
+    return A.Compose(
+        pipeline,
+        additional_targets={'target': 'image'}
+    )
